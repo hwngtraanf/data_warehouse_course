5b11f72dbe23d7102f5880af54cb1d65b2248f41
huntra01 <hung.tranvan@trustonic.com>
2022-11-18 22:27:16 +0700

    done lesson-0104b + fix tests for fact_sales_order_line

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,7 @@
 SELECT 
-  order_line_id AS sales_order_line_key
-  , stock_item_id as product_key
-  , quantity
-  , unit_price
-  , quantity * unit_price AS gross_amount
+  CAST(order_line_id AS INTEGER) AS sales_order_line_key
+  , CAST(stock_item_id AS INTEGER) AS product_key
+  , CAST(quantity AS INTEGER) AS quantity
+  , CAST(unit_price AS NUMERIC) AS unit_price
+  , CAST(quantity AS INTEGER) * CAST(unit_price AS NUMERIC) AS gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
