2c25df0dbc7b583f917f9a63d88a700c82da8345
huntra01 <hung.tranvan@trustonic.com>
2022-11-18 22:41:19 +0700

    done lesson-0105a

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,23 @@
+WITH 
+  dim_product__source AS (
+    SELECT * 
+    FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  ),
+  dim_product__rename_column AS (
+    SELECT
+      stock_item_id AS product_key
+      , stock_item_name AS product_name
+      , brand AS brand_name
+    FROM dim_product__source
+  ),
+  dim_product__cast_type AS (
+    SELECT 
+      CAST(product_key AS INTEGER) AS product_key
+      , CAST(product_name AS STRING) AS product_name
+      , CAST(brand_name AS STRING) AS brand_name
+    FROM dim_product__rename_column
+  )
+
 SELECT 
-  CAST(stock_item_id AS INTEGER) AS product_key
-  , CAST(stock_item_name AS STIRNG) AS product_name
-  , CAST(brand AS STIRNG) AS brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  *
+FROM dim_product__cast_type
