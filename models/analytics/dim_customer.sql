02bdaeb1b2f2ccad2bb40fada5c1b64750cd03de
huntra01 <hung.tranvan@trustonic.com>
2022-11-19 16:18:05 +0700

    fix cte name on lesson-0106a

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,25 +1,25 @@
-WITH dim_sales_customers__source AS (
+WITH dim_customer__source AS (
   SELECT 
     *
   FROM `vit-lam-data.wide_world_importers.sales__customers`
 )
 
-, dim_sales_customers__rename_columns AS (
+, dim_customer__rename_columns AS (
   SELECT
     customer_id AS customer_key
     , customer_name 
-  FROM dim_sales_customers__source
+  FROM dim_customer__source
 )
 
-, dim_sales_customers__cast_type AS (
+, dim_customer__cast_type AS (
   SELECT
     CAST(customer_key AS INTEGER) AS customer_key
     , CAST(customer_name AS STRING) AS customer_name
-  FROM dim_sales_customers__rename_columns
+  FROM dim_customer__rename_columns
 )
 
 SELECT
   customer_key
   , customer_name
-FROM dim_sales_customers__cast_type
+FROM dim_customer__cast_type
 
