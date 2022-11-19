5c7e450078a8e35b7fda7f8d72ead3e73da8d5cf
huntra01 <hung.tranvan@trustonic.com>
2022-11-19 16:14:44 +0700

    done lesson-0106a

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,25 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+WITH dim_sales_customers__source AS (
+  SELECT 
+    *
+  FROM `vit-lam-data.wide_world_importers.sales__customers`
+)
+
+, dim_sales_customers__rename_columns AS (
+  SELECT
+    customer_id AS customer_key
+    , customer_name 
+  FROM dim_sales_customers__source
+)
+
+, dim_sales_customers__cast_type AS (
+  SELECT
+    CAST(customer_key AS INTEGER) AS customer_key
+    , CAST(customer_name AS STRING) AS customer_name
+  FROM dim_sales_customers__rename_columns
+)
+
+SELECT
+  customer_key
+  , customer_name
+FROM dim_sales_customers__cast_type
+
