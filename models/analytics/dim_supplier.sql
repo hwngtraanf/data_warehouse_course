d50ab214965530b2d7332fef0aa9e25ed2f1d993
huntra01 <hung.tranvan@trustonic.com>
2022-11-23 19:15:30 +0700

    done lesson 0107a

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,25 @@
+WITH dim_supplier__source AS (
+  SELECT
+    *
+  FROM
+    `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+
+, dim_supplier__rename_columns AS (
+  SELECT
+    supplier_id AS supplier_key
+    , supplier_name
+  FROM dim_supplier__source
+)
+
+, dim_supplier__cast_type AS (
+  SELECT 
+    CAST(supplier_key AS INTEGER) AS supplier_key
+    , CAST(supplier_name AS STRING) AS supplier_name
+  FROM dim_supplier__rename_columns
+)
+
 SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+  supplier_key
+  , supplier_name
+FROM dim_supplier__cast_type
