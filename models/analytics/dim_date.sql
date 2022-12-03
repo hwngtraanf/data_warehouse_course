b53412c7bfa734ce17aef235c749c2eb91771865
huntra01 <hung.tranvan@trustonic.com>
2022-12-03 12:13:32 +0700

    done lesson 0111b

---
+++ models/analytics/dim_date.sql
@@ -0,0 +1,16 @@
+SELECT
+  d AS date
+  , FORMAT_DATE('%A', d) AS day_of_week
+  , FORMAT_DATE('%a', d) AS day_of_week_short
+  , CASE 
+      WHEN FORMAT_DATE('%A', d) IN ('Sunday', 'Saturday') THEN 'Weekend' ELSE 'Weekday' 
+    END AS is_weekday_or_weekend
+  , DATE_TRUNC(d, MONTH) AS year_month
+  , FORMAT_DATE('%B', d) AS month
+  , DATE_TRUNC(d, YEAR) AS year
+  , EXTRACT(YEAR FROM d) AS year_number
+FROM (
+  SELECT
+    *
+  FROM
+    UNNEST(GENERATE_DATE_ARRAY('2014-01-01', '2050-01-01', INTERVAL 1 DAY)) AS d )
