f2e3e41c56dd099869a79cf03805e79b3174ef1b
huntra01 <hung.tranvan@trustonic.com>
2022-12-12 22:51:27 +0700

    done fact_sales_order_line

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,57 +1,90 @@
-WITH fact_sales_order_line__source AS (
-  SELECT * 
-  FROM `vit-lam-data.wide_world_importers.sales__order_lines`
-)
+  WITH fact_sales_order_line__source AS (
+    SELECT * 
+    FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+  )
 
-, fact_sales_order_line__rename_column AS (
+  , fact_sales_order_line__rename_column AS (
+    SELECT 
+      order_id AS sales_order_key
+      , description
+      , order_line_id AS sales_order_line_key
+      , stock_item_id AS product_key
+      , package_type_id AS package_type_key
+      , quantity 
+      , unit_price
+      , tax_rate
+      , picked_quantity
+      , picking_completed_when
+    FROM 
+      fact_sales_order_line__source
+  )
+
+  , fact_sales_order_line__cast_type AS (
+    SELECT 
+      CAST(sales_order_key AS INTEGER) AS sales_order_key
+      , CAST(description AS STRING) AS description
+      , CAST(sales_order_line_key AS INTEGER) AS sales_order_line_key
+      , CAST(product_key AS INTEGER) AS product_key
+      , CAST(package_type_key AS INTEGER) AS package_type_key
+      , CAST(quantity AS INTEGER) AS quantity
+      , CAST(unit_price AS NUMERIC) AS unit_price
+      , CAST(tax_rate AS NUMERIC) AS tax_rate
+      , CAST(picked_quantity AS INTEGER) AS picked_quantity
+      , CAST(picking_completed_when AS DATE) AS picking_completed_when
+    FROM 
+      fact_sales_order_line__rename_column
+  )
+
+  , fact_sales_order_line__calculate_measure AS (
+    SELECT
+      *
+      , quantity * unit_price AS gross_amount
+    FROM 
+      fact_sales_order_line__cast_type
+  )
+
+  , fact_sales_order_line__join_stg_fact_sales_order AS (
+    SELECT
+      fact_line.*
+      , fact_header.customer_key
+      , COALESCE(fact_header.salesperson_person_key, -1) AS salesperson_person_key
+      , COALESCE(fact_header.picked_by_person_key, -1) AS picked_by_person_key
+      , COALESCE(fact_header.contact_person_key, -1) AS contact_person_key
+      , fact_header.backorder_order_key
+      , fact_header.order_date
+      , fact_header.expected_delivery_date
+      , fact_header.customer_purchase_order_number
+      , fact_header.is_undersupply_backordered
+      , fact_header.comments
+      , fact_header.delivery_instructions
+      , fact_header.internal_comments
+    FROM fact_sales_order_line__calculate_measure fact_line
+    LEFT JOIN {{ ref('stg_fact_sales_order') }} fact_header
+    ON fact_line.sales_order_key = fact_header.sales_order_key
+  )
+
   SELECT 
-    order_id AS sales_order_key
-    , order_line_id AS sales_order_line_key
-    , stock_item_id AS product_key
-    , quantity 
+    sales_order_line_key
+    , description
+    , sales_order_key
+    , customer_key
+    , product_key
+    , salesperson_person_key
+    , picked_by_person_key
+    , contact_person_key
+    , backorder_order_key
+    , package_type_key
+    , order_date
+    , expected_delivery_date
+    , customer_purchase_order_number
+    , is_undersupply_backordered
+    , comments
+    , delivery_instructions
+    , internal_comments
+    , quantity
     , unit_price
-  FROM 
-    fact_sales_order_line__source
-)
-
-, fact_sales_order_line__cast_type AS (
-  SELECT 
-    CAST(sales_order_key AS INTEGER) AS sales_order_key
-    , CAST(sales_order_line_key AS INTEGER) AS sales_order_line_key
-    , CAST(product_key AS INTEGER) AS product_key
-    , CAST(quantity AS INTEGER) AS quantity
-    , CAST(unit_price AS NUMERIC) AS unit_price
-  FROM 
-    fact_sales_order_line__rename_column
-)
-
-, fact_sales_order_line__calculate_measure AS (
-  SELECT
-    *
-    , quantity * unit_price AS gross_amount
-  FROM 
-    fact_sales_order_line__cast_type
-)
-
-, fact_sales_order_line__join_stg_fact_sales_order AS (
-  SELECT
-    fact_line.*
-    , fact_header.customer_key
-    , COALESCE(fact_header.picked_by_person_key, -1) AS picked_by_person_key
-    , fact_header.order_date
-  FROM fact_sales_order_line__calculate_measure fact_line
-  LEFT JOIN {{ ref('stg_fact_sales_order') }} fact_header
-  ON fact_line.sales_order_key = fact_header.sales_order_key
-)
-
-SELECT 
-  sales_order_line_key
-  , sales_order_key
-  , customer_key
-  , product_key
-  , picked_by_person_key
-  , quantity
-  , unit_price
-  , gross_amount
-  , order_date
-FROM fact_sales_order_line__join_stg_fact_sales_order
+    , gross_amount
+    , tax_rate
+    , picked_quantity
+    , picking_completed_when
+  FROM fact_sales_order_line__join_stg_fact_sales_order
--- models/staging/stg_fact_sales_order.sql
+++ models/staging/stg_fact_sales_order.sql
@@ -8,8 +8,18 @@ WITH stg_fact_sales_orders__source AS (
   SELECT
     order_id AS sales_order_key
     , customer_id AS customer_key
+    , salesperson_person_id AS salesperson_person_key
     , picked_by_person_id AS picked_by_person_key
+    , contact_person_id AS contact_person_key
+    , backorder_order_id AS backorder_order_key
     , order_date
+    , expected_delivery_date
+    , customer_purchase_order_number
+    , is_undersupply_backordered
+    , comments
+    , delivery_instructions
+    , internal_comments
+    , picking_completed_when
   FROM stg_fact_sales_orders__source
 )
 
@@ -17,14 +27,40 @@ WITH stg_fact_sales_orders__source AS (
   SELECT
     CAST(sales_order_key AS INTEGER) AS sales_order_key
     , CAST(customer_key AS INTEGER) AS customer_key
+    , CAST(salesperson_person_key AS INTEGER) AS salesperson_person_key
     , CAST(picked_by_person_key AS INTEGER) AS picked_by_person_key
+    , CAST(contact_person_key AS INTEGER) AS contact_person_key
+    , CAST(backorder_order_key AS INTEGER) AS backorder_order_key
     , CAST(order_date AS DATE) AS order_date
+    , CAST(expected_delivery_date AS DATE) AS expected_delivery_date
+    , CAST(customer_purchase_order_number AS INTEGER) AS customer_purchase_order_number
+    , CAST(is_undersupply_backordered AS BOOLEAN) AS is_undersupply_backordered_boolean
+    , CAST(comments AS STRING) AS comments
+    , CAST(delivery_instructions AS STRING) AS delivery_instructions
+    , CAST(internal_comments AS STRING) AS internal_comments
+    , CAST(picking_completed_when AS DATE) AS picking_completed_when
   FROM stg_fact_sales_orders__rename_columns
 )
 
 SELECT
   sales_order_key
   , customer_key
+  , COALESCE(salesperson_person_key, 0) AS salesperson_person_key
   , COALESCE(picked_by_person_key, 0) AS picked_by_person_key
+  , COALESCE(contact_person_key, 0) AS contact_person_key
+  , backorder_order_key
   , order_date
+  , expected_delivery_date
+  , customer_purchase_order_number
+  , CASE 
+          WHEN is_undersupply_backordered_boolean is TRUE
+            THEN 'Undersupply Backordered'
+          WHEN is_undersupply_backordered_boolean is FALSE 
+            THEN 'Not Undersupply Backordered' 
+          ELSE 'Undefined' END
+      AS is_undersupply_backordered
+  , COALESCE(comments, 'Undefined') AS comments
+  , COALESCE(delivery_instructions, 'Undefined') AS delivery_instructions
+  , COALESCE(internal_comments, 'Undefined') AS internal_comments
+  , picking_completed_when
 FROM stg_fact_sales_orders__cast_type
